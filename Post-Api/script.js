b8ab5ee641a828586f94c83346a1da9cc2e9dbc5
rmCoder7 <mohammedayon2021@gmail.com>
2023-11-15 22:57:10 +0600

    add

--- Post-Api/script.js
+++ Post-Api/script.js
@@ -13,13 +13,24 @@ function displayPost(posts)
 
     // console.log(data);
 
+  const postIDCalling = document.getElementById('posts-container')
+
     for(const post of posts)
     {
-    //   console.log(post);
+      console.log(post);
 
+    const articales = document.createElement('div')
+    articales.classList.add('post')
+    articales.innerHTML = `
     
+    <h2>User- ${post.userId}</h2>
+    <h3>Post: ${post.title}</h3>
+    <p>Post Description : ${post.body} </p>
+    
+    
+    `;
               
-
+postIDCalling.appendChild(articales);
     }
 
 
